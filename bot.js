aa6a1d1bf16af20fedf54673c31f50f5b23b7ff2
malkhasan <45199097+malkhasan@users.noreply.github.com>
2019-01-02 21:38:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`System II +help`,"https://www.twitch.tv/ahmad107996")
+client.user.setGame(`System II #help`,"https://www.twitch.tv/ahmad107996")
 client.user.setStatus("dnd")
 });
 
