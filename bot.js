71235f6efdd7e68ee415dd6fb89bd72f95b89ba7
malkhasan <45199097+malkhasan@users.noreply.github.com>
2019-01-06 13:29:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -835,35 +835,6 @@ client.on("guildMemberAdd", member => {
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/////////////////////////
-////////////////////////
- client.on('message', message => {
-  var prefix = "#"; /// غير البرفيكس
-  
-if (message.author.bot) return;
-if (!message.content.startsWith(prefix)) return;
-   
-let command = message.content.split(" ")[0];
-command = command.slice(prefix.length);
-   
-let args = message.content.split(" ").slice(1);
-   
-if (command === "say") {
-message.delete()
-  message.channel.sendMessage(args.join(" ")).catch(console.error);
-}
+ 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
